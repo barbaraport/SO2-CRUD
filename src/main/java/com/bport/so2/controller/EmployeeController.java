747767c3b0269eeea0ecdc4eb5fd4b0551757917
Bárbara Port <barbara.port@outlook.com>
2021-09-04 20:56:31 -0300

    add employee

--- src/main/java/com/bport/so2/controller/EmployeeController.java
+++ src/main/java/com/bport/so2/controller/EmployeeController.java
@@ -3,7 +3,13 @@ package com.bport.so2.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bport.so2.model.Employee;
@@ -19,5 +25,11 @@ public class EmployeeController {
 	@GetMapping("/getAllEmployees")
 	List<Employee> allEmployees () {
 		return employeeRepository.findAll();
+	}
+	
+	@PostMapping("/addEmployee")
+	HttpStatus addEmployee (@RequestBody Employee employee) {
+		employeeRepository.save(employee);
+		return HttpStatus.OK;
 	}
 }
